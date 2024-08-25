0b76bb37d19796fdc0d3f3b8cd96598e50d59dc3
anderson9527-max <t5960667@livemail.tw>
2024-08-24 23:19:26 -0900

    Add files via upload

---
+++ 0722練習.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+name="jake"
+age=34
+rate=0.18
+status=True#布林值 booleanla
+status=False
+print(name)
+print(age)
+print(rate)
+print(status)
+
+'''
+#單行註解
+"""
+整段註解
+"""
+'''
+a=10
+b=2
+print(a+b)
+print(b-a)
+print(a*b)
+print(a/b)#會有小數點
+print(a//b)#取商
+print(a%b)#取餘數
+print(a**b)#次方
+
+
+
+#比較運算子  符號順序不能變
+10>3
+10>=3
+10<=3
+10==3
+10!=3
+a=10
+b=5
+print(a=b)
+print(a>b)
+print(a>=b*2)
+print(a!=b)
+print(a==b*2)
+
+#邏輯運算子 and且 or或  not相反 &&記憶體 //
+a=10
+b=5
+print(a>b or a==b)
+print(not(a>b))
+print(a>=b and a==b*2)
+print(a>b and a==b)
+
+#指定運算子
+a=10
+a=a+2
+print(a)
+a+=10
+print(a)
+a-=6
+print(a)
+a*=2
+print(a)
+a/=3
+print(a)
+a%=3
+print(a)
+
+'''
+a=10
+a=a+2
+print(a)12
+a+=10
+print(a)22
+a-=6
+print(a)16
+a*=2
+print(a)32
+a/=3
+print(a)10.66666
+a%=3
+print(a)1.66666#10.666除以3的餘是1.666
+'''
+
+print(10,20,30,40)
+print("現金:",100,"元")
+print(10,20,30,40,sep="-")
+print(1,2,3,4,5,sep='')
+print(1,2,3,4,5,sep='&',end='a')
+print('hello')
+
+"""
+if布林判斷式:成立時要執行的區段
+if面要縮排,條件要整齊
+"""
+
+
+age=22
+
+if age>=18
+    print("考駕照") 
+    print("看限制級")
+print("執行完畢")
+
+
+
+
+
+
